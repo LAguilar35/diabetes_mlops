c9913d3781cf1edae3a0f8ba808454cce2167e9f
laguilar <baal.siul@gmail.com>
2024-11-15 13:16:23 -0600

    Dockerizado + FastAPI

--- diabetes_mlops/main.py
+++ diabetes_mlops/main.py
@@ -1,38 +1,47 @@
 from typing import List
 from fastapi import FastAPI, HTTPException
-import pickle
+import joblib
 from pydantic import BaseModel
 import uvicorn
+import pandas as pd
 
 with open('trained_model.pkl', "rb") as f:
-    model = pickle.load(f)
+    model = joblib.load(f)
 
 class DiabetesData(BaseModel):
-    features = List[float]
-    # Age: int
-    # Gender: str
-    # Polyuria: str
-    # Polydipsia: str
-    # sudden_weight_loss: str
-    # weakness: str
-    # Polyphagia: str
-    # Genital_thrush: str
-    # visual_blurring: str
-    # Itching: str
-    # Irritability: str
-    # delayed_healing: str
-    # partial_paresis: str
-    # muscle_stiffness: str
-    # Alopecia: str
-    # Obesity: str
-    # result: bool
+    Age: int
+    Gender: str
+    Polyuria: str
+    Polydipsia: str
+    sudden_weight_loss: str
+    weakness: str
+    Polyphagia: str
+    Genital_thrush: str
+    visual_blurring: str
+    Itching: str
+    Irritability: str
+    delayed_healing: str
+    partial_paresis: str
+    muscle_stiffness: str
+    Alopecia: str
+    Obesity: str
 
 app = FastAPI()
 
 @app.post("/predict")
 def predict(data: DiabetesData):
-    pred = model.predict([data])
-    return {"result": int(pred)}    
+    new_data = [data.Age, data.Gender, data.Polyuria, data.Polydipsia, data.sudden_weight_loss, 
+                data.weakness, data.Polyphagia, data.Genital_thrush, data.visual_blurring, data.Itching, 
+                data.Irritability, data.delayed_healing, data.partial_paresis, data.muscle_stiffness, 
+                data.Alopecia, data.Obesity]
+    cols = ['Age','Gender','Polyuria','Polydipsia','sudden weight loss',
+            'weakness','Polyphagia','Genital thrush','visual blurring','Itching',
+            'Irritability','delayed healing','partial paresis','muscle stiffness','Alopecia',
+            'Obesity']
+    df = pd.DataFrame([new_data], columns=cols)
+
+    pred = model.predict(df)
+    return {"result": "Diabetes: {}".format('Yes' if int(pred) == 1 else 'No')}    
 
 
 @app.get('/')
